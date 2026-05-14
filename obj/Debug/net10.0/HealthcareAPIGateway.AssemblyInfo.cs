b4e461068f81fb3663e2169881cbca36f37178a8
LAKSHMIPATHI1807 <lakshmipathimangu1681861@gmail.com>
2026-05-14 15:23:55 +0530

    1

--- obj/Debug/net10.0/HealthcareAPIGateway.AssemblyInfo.cs
+++ obj/Debug/net10.0/HealthcareAPIGateway.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HealthcareAPIGateway")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8723adf3318417fa563266dd132de87103f4b753")]
 [assembly: System.Reflection.AssemblyProductAttribute("HealthcareAPIGateway")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HealthcareAPIGateway")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
